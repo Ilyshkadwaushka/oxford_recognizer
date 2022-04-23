0945352ae7392ef0daaa5d22bc1b9fdbfad33ef2
Илья Фурман <ilafurman@MacBook-Pro-Ila.local>
2022-04-23 19:23:21 +0300

    Change router

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
    history: createWebHistory(),
    routes: [
       {
-         path: '/',
+         path: '/oxford_recognizer/',
          component: Landing,
          /*
          meta: {
